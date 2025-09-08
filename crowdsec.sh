2149223bdf7e577d0e382f9dda49decbcae47a04
Michael Lorenzo <61134276+michael-lorenzo@users.noreply.github.com>
2025-09-08 14:04:15 -0700

    Add installation instructions for CrowdSec

--- crowdsec.sh
+++ crowdsec.sh
@@ -1,4 +1,5 @@
 # https://doc.crowdsec.net/u/getting_started/installation/linux
+
 curl -s https://install.crowdsec.net | sudo sh
 apt-get -y install crowdsec crowdsec-firewall-bouncer-iptables
 systemctl restart crowdsec
