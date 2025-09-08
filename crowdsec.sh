d7fd6e968dd56c7312744ba3d6f7282fd9cc2b58
Michael Lorenzo <61134276+michael-lorenzo@users.noreply.github.com>
2025-09-08 13:10:05 -0700

    Restart CrowdSec service after installation

--- crowdsec.sh
+++ crowdsec.sh
@@ -1,3 +1,4 @@
 # https://doc.crowdsec.net/u/getting_started/installation/linux
 curl -s https://install.crowdsec.net | sudo sh
 apt-get install -y crowdsec crowdsec-firewall-bouncer-iptables
+systemctl restart crowdsec
