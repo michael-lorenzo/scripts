e07b441bfd0c75889fef7cf3b049157292ccb02d
Michael Lorenzo <61134276+michael-lorenzo@users.noreply.github.com>
2025-09-08 13:54:53 -0700

    Fix apt-get install command syntax

--- crowdsec.sh
+++ crowdsec.sh
@@ -1,5 +1,5 @@
 # https://doc.crowdsec.net/u/getting_started/installation/linux
 curl -s https://install.crowdsec.net | sudo sh
-apt-get install -y crowdsec crowdsec-firewall-bouncer-iptables
+apt-get -y install crowdsec crowdsec-firewall-bouncer-iptables
 systemctl restart crowdsec
 # cscli decisions list
