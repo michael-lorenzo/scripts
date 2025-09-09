db840585f17a1cdd02f54b1632b5871df814bed4
Michael Lorenzo <61134276+michael-lorenzo@users.noreply.github.com>
2025-09-09 12:07:54 -0700

    Remove sudo from Go installation script

--- go.sh
+++ go.sh
@@ -3,7 +3,7 @@
 version="1.25.1"
 file_name="go${version}.linux-amd64.tar.gz"
 wget "https://go.dev/dl/${file_name}"
-sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf $file_name
+rm -rf /usr/local/go && tar -C /usr/local -xzf $file_name
 rm $file_name
 echo 'export GOPATH=~/.go' >> ~/.bashrc
 echo 'export PATH=$PATH:/usr/local/go/bin:~/.go/bin' >> ~/.bashrc
