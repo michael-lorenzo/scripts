2ace2877911650f9bb4335c0d31a7b0f045751eb
Michael Lorenzo <61134276+michael-lorenzo@users.noreply.github.com>
2025-09-09 02:10:16 -0700

    Add script to install Go

---
+++ go.sh
@@ -0,0 +1,11 @@
+# https://go.dev/doc/install
+
+version="1.25.1"
+file_name="go${version}.linux-amd64.tar.gz"
+wget "https://go.dev/dl/${file_name}"
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf $file_name
+rm $file_name
+echo 'export GOPATH=~/.go' >> ~/.bashrc
+echo 'export PATH=$PATH:/usr/local/go/bin:~/.go/bin' >> ~/.bashrc
+source ~/.bashrc
+go install golang.org/x/tools/gopls@latest
