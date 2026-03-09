ff35a38273153890a1536194addb10c2fb5620ca
Michael Lorenzo <61134276+michael-lorenzo@users.noreply.github.com>
2026-03-09 14:43:02 -0700

    Update go.sh

--- src/langs/go.sh
+++ src/langs/go.sh
@@ -6,10 +6,10 @@ file_name="$(curl -fsSL https://go.dev/dl/?mode=json | jq -r '.[0].files[] | sel
 wget "https://go.dev/dl/${file_name}"
 sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf "$file_name"
 rm "$file_name"
-
 if ! grep -qxF 'export PATH="$PATH:/usr/local/go/bin"' ~/.bashrc 2>/dev/null; then
   echo $'\n# Go' >> ~/.bashrc
   echo 'export GOPATH="$HOME/.local/share/go"' >> ~/.bashrc
   echo 'export GOBIN="$HOME/.local/bin"' >> ~/.bashrc
   echo 'export PATH="$PATH:/usr/local/go/bin"' >> ~/.bashrc
 fi
+go install golang.org/x/tools/gopls@latest
