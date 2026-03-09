ee96f9c9285cdc4cd0029962f7797d537f7c14ce
Michael Lorenzo <61134276+michael-lorenzo@users.noreply.github.com>
2026-03-09 19:01:58 +0000

    .

--- src/js/bun.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# https://github.com/oven-sh/bun
-
-apt-get -y install curl unzip
-curl -fsSL https://bun.com/install | bash
-source ~/.bashrc
-
-# bun upgrade
--- src/python/uv.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# https://github.com/astral-sh/uv
-
-curl -LsSf https://astral.sh/uv/install.sh | sh
-
-# uv self update
