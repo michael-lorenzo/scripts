da0cf6bf3b085565c5b732cfa5c3d274339066eb
Michael Lorenzo <61134276+michael-lorenzo@users.noreply.github.com>
2026-03-09 15:02:13 -0700

    Update go.sh

--- src/langs/go.sh
+++ src/langs/go.sh
@@ -8,7 +8,7 @@ sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf "$file_name"
 rm "$file_name"
 if ! grep -qxF 'export PATH="$PATH:/usr/local/go/bin"' ~/.bashrc 2>/dev/null; then
   echo $'\n# Go' >> ~/.bashrc
-  echo 'export GOPATH="$HOME/.local/go"' >> ~/.bashrc
+  echo 'export GOPATH="$HOME/.local/share/go"' >> ~/.bashrc
   echo 'export GOBIN="$HOME/.local/bin"' >> ~/.bashrc
   echo 'export PATH="$PATH:/usr/local/go/bin"' >> ~/.bashrc
 fi
