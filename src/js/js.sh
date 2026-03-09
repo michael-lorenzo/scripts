eb18484e1ef67e3f7033ddf6723b4efee52d4f8a
Michael Lorenzo <61134276+michael-lorenzo@users.noreply.github.com>
2026-03-09 04:07:13 -0700

    Update and rename node.sh to js.sh

--- src/js/js.sh
+++ src/js/js.sh
@@ -3,9 +3,11 @@
 # https://github.com/Schniz/fnm
 # https://github.com/nodejs/node
 # https://github.com/pnpm/pnpm
+# https://github.com/oven-sh/bun
 
 sudo apt-get update && sudo apt-get -y install curl unzip ca-certificates
 curl -fsSL https://fnm.vercel.app/install | bash
 export PATH="$HOME/.local/share/fnm:$PATH" && eval "$(fnm env)"
 fnm install --lts
 npm install -g npm pnpm
+curl -fsSL https://bun.com/install | bash
