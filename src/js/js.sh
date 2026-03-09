0d2aca061fa3f9b5e1c24a4c30f9ba4511bbb734
Michael Lorenzo <61134276+michael-lorenzo@users.noreply.github.com>
2026-03-09 04:57:35 -0700

    Update js.sh

--- src/js/js.sh
+++ src/js/js.sh
@@ -8,5 +8,5 @@ sudo apt-get update && sudo apt-get -y install curl unzip ca-certificates
 curl -fsSL https://fnm.vercel.app/install | bash
 export PATH="$HOME/.local/share/fnm:$PATH" && eval "$(fnm env)"
 fnm install --lts
-npm install -g corepack && corepack enable
+npm install -g npm corepack && corepack enable
 curl -fsSL https://bun.com/install | bash
