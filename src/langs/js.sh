e725b28adaad9c7fa0245aea4a0ececd50465686
Michael Lorenzo <61134276+michael-lorenzo@users.noreply.github.com>
2026-03-10 16:06:45 -0700

    Update js.sh to install without curl
    Removed 'curl' from the installation command for js.sh.

--- src/langs/js.sh
+++ src/langs/js.sh
@@ -4,7 +4,7 @@
 # https://github.com/nodejs/corepack
 # https://github.com/oven-sh/bun
 
-sudo apt-get update && sudo apt-get -y install curl unzip ca-certificates
+sudo apt-get update && sudo apt-get -y install unzip
 curl -fsSL https://fnm.vercel.app/install | bash
 source ~/.bashrc
 fnm install --lts
