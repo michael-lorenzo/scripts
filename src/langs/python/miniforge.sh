d3a03ea732712cb2ae7a2826bcf7f2c289b9d2a5
Michael Lorenzo <61134276+michael-lorenzo@users.noreply.github.com>
2026-03-10 16:18:31 -0700

    Change Miniforge installation path to ~/.local/miniforge

--- src/langs/python/miniforge.sh
+++ src/langs/python/miniforge.sh
@@ -3,9 +3,9 @@
 # https://github.com/conda-forge/miniforge
 
 wget -O Miniforge3.sh "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
-bash Miniforge3.sh -b -p ~/.miniforge
-~/.miniforge/bin/conda config --set auto_activate_base false
-~/.miniforge/bin/conda init
+bash Miniforge3.sh -b -p ~/.local/miniforge
+~/.local/miniforge/bin/conda config --set auto_activate_base false
+~/.local/miniforge/bin/conda init
 source ~/.bashrc
 conda update --all -y # WARNING: Requires a lot of RAM / Swap
 conda clean --all -y
