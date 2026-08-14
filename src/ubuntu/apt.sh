cd813d7629b1a3dccd6a06e589944a8bb7828d8b
Michael Lorenzo <61134276+michael-lorenzo@users.noreply.github.com>
2026-08-14 16:14:13 -0700

    Install systemd-container and Docker in apt.sh
    Added installation of systemd-container and Docker.

--- src/ubuntu/apt.sh
+++ src/ubuntu/apt.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env -S sudo bash
 
 apt-get update
-apt-get -y install mosh
+apt-get -y install mosh systemd-container
 DEBIAN_FRONTEND=noninteractive apt-get -y dist-upgrade
 apt-get -y autopurge snapd
 apt-get clean
+curl -fsSL https://get.docker.com | sh
 reboot
