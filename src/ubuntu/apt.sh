8e503608440eb1a12408a8dd62f26a2eff48e659
Michael Lorenzo <61134276+michael-lorenzo@users.noreply.github.com>
2026-08-15 10:56:37 -0700

    Update apt.sh

--- src/ubuntu/apt.sh
+++ src/ubuntu/apt.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env -S sudo bash
 
 apt-get update
-apt-get -y install mosh systemd-container
+apt-get -y install mosh systemd-container btrfs-progs
 DEBIAN_FRONTEND=noninteractive apt-get -y dist-upgrade
 apt-get -y autopurge snapd
 apt-get clean
